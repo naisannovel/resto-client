48b7cad76b9ccda279fc4e975ddbb6ee2562cf20
naisannovel <naisannovel@gmail.com>
2021-09-24 19:47:37 +0600

    update

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -10,14 +10,14 @@ const About = () => {
         <h3>why choose us?</h3>
       </div>
       <div className="row">
-        <div className="col-md-6 border border-danger">
+        <div className="col-md-6 d-flex justify-content-center align-items-center">
           <img
             src="assets/images/about.png"
             style={{ width: "100%", height: "100%" }}
             alt="aboutImage"
           />
         </div>
-        <div className="col-md-6 about__content__container border border-danger">
+        <div className="col-md-6 about__content__container">
           <h3>best food in the country</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
@@ -30,15 +30,15 @@ const About = () => {
             nemo. Sit porro illo eos cumque deleniti iste alias, eum natus.
           </p>
           <div className="about__icons__container">
-            <div className="icons">
+            <div className="about__icons">
               <FontAwesomeIcon icon={faTruck} />
               <span>free delivery</span>
             </div>
-            <div className="icons">
+            <div className="about__icons">
               <FontAwesomeIcon icon={faDollarSign} />
               <span>easy payments</span>
             </div>
-            <div className="icons">
+            <div className="about__icons">
               <FontAwesomeIcon icon={faHeadset} />
               <span>24/7 service</span>
             </div>
